b0cece8ce683d175fa8b5f50ef9b5ae24119d8f6
carolina <carolina@slac.stanford.edu>
2017-10-29 15:33:39 -0700

    LCLSII EIC: adding all files

--- Keithley6482TestApp/src/O.linuxRT-x86_64/Keithley6482Test_registerRecordDeviceDriver.cpp
+++ Keithley6482TestApp/src/O.linuxRT-x86_64/Keithley6482Test_registerRecordDeviceDriver.cpp
@@ -308,7 +308,7 @@ static struct iocshVarDef vardefs[] = {
 int Keithley6482Test_registerRecordDeviceDriver(DBBASE *pbase)
 {
     static int executed = 0;
-    const char *bldTop = "/afs/slac.stanford.edu/u/cd/carolina/PICOAMMETER_TEST/Keithley6482/Keithley6482-git";
+    const char *bldTop = "/afs/slac.stanford.edu/u/cd/carolina/GITTING/Keithley6482";
     const char *envTop = getenv("TOP");
 
     if (envTop && strcmp(envTop, bldTop)) {
